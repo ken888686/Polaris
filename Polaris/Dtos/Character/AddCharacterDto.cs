1014403954780b7e31b53be2c2fd272062afecc2
Aaron <ken888686@gmail.com>
2022-12-03 21:57:46 +0900

    create dtos

---
+++ Polaris/Dtos/Character/AddCharacterDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Polaris.Dtos.Character
+{
+        public class AddCharacterDto
+        {
+                public string Name { get; set; } = "Unknown";
+                public int HitPoint { get; set; } = 0;
+                public int Strenth { get; set; } = 0;
+                public int Defense { get; set; } = 0;
+                public int Intelligence { get; set; } = 0;
+                public RpgClass Class { get; set; } = RpgClass.Knight;
+        }
+}
---
+++ Polaris/Dtos/Character/GetCharacterDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Polaris.Dtos.Character
+{
+        public class GetCharacterDto
+        {
+                public int Id { get; set; } = 0;
+                public string Name { get; set; } = "Unknown";
+                public int HitPoint { get; set; } = 0;
+                public int Strenth { get; set; } = 0;
+                public int Defense { get; set; } = 0;
+                public int Intelligence { get; set; } = 0;
+                public RpgClass Class { get; set; } = RpgClass.Knight;
+        }
+}
