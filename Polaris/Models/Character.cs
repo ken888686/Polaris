f31b6e8a840024b890ea028869781202eeafe328
Aaron <ken888686@gmail.com>
2022-12-05 22:05:28 +0900

    create User table

--- Polaris/Models/Character.cs
+++ Polaris/Models/Character.cs
@@ -5,9 +5,10 @@
         public int Id { get; set; } = 0;
         public string Name { get; set; } = "Unknown";
         public int HitPoint { get; set; } = 0;
-        public int Strenth { get; set; } = 0;
+        public int Strength { get; set; } = 0;
         public int Defense { get; set; } = 0;
         public int Intelligence { get; set; } = 0;
         public RpgClass Class { get; set; } = RpgClass.Knight;
+        public User? User { get; set; }
     }
 }
